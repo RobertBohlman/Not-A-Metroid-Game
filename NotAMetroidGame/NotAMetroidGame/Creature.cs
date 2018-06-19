c0da75429a1ea206561bdb0963c6ad2174d45e25
RobertBohlman <robertbohlman@hotmail.com>
2018-06-18 19:37:11 -0700

    Added the initial class structure

---
+++ NotAMetroidGame/NotAMetroidGame/Creature.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Creature : Entity {
+    //This may or may not stay as an int
+    private int HP;
+
+    //Reduces damage taken
+    private int armor;
+
+	public Creature() {
+	}
+
+    /** Adjust this creature's position by adding the movement vector.
+     * 
+     * This function will likely be where the logic for collision is implemented.
+     * 
+     **/
+    public void Move(Vector2D movement) {
+
+    }
+
+    /** Deals X damage to this creature's HP, returns true if this damage is lethal.
+     * 
+     * Example: Player lands a hit on enemy x, code checks the stats on the 
+     * player's weapon, charatcer, etc and calls x.Damage(howevermuch damage should be done).
+     * This function takes into account armor and other stats we decide to consider.
+     * 
+     * Some enemies may have special effects or resistances when they take damage.
+     * For this instance, that enemy can override this function.
+     * 
+     **/
+    public bool Damage(long damage) {
+
+    }
+
+    //*Insert Zelda CD-I reference here*
+    public void Die() {
+
+    }
+}
---
+++ NotAMetroidGame/NotAMetroidGame/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Entity {
+    //The entity's current position
+    private Vector2 position;
+
+    //See note for draw function
+    private Texture2D sprite;
+
+    public Entity() {
+	}
+
+    /** Draw the entity on screen.
+     * 
+     *  The draw function will use the sprite field 
+     *  and other necessary calls to draw this entity 
+     *  on screen at its current position
+     *  
+     *  [NOTE]: While using a Texture2D allows us to draw a static image to the screen, 
+     *  I'm not sure if there's a better way to draw frames of animation (there probably is)
+     *  As a result, this will probably get revised slightly.
+     **/
+    public void Draw() {
+
+    }
+}
